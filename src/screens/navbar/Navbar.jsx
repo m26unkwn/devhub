99f12b4596dd8ccc69c2cd0a59bd65b75418a134
m26unkwn <shukla9915294376@gmail.com>
2022-04-19 18:31:49 +0530

    RESPONSIVE: ADDED RESPONSIVE

--- src/screens/navbar/Navbar.jsx
+++ src/screens/navbar/Navbar.jsx
@@ -2,12 +2,14 @@
 
 import { Header } from "./header/Header";
 import { Sidebar } from "./sidebar/Sidebar";
+import { MobileNav } from "./mobile/MobileNav";
 
 export const Navbar = () => {
   return (
     <>
       <Header />
       <Sidebar />
+      <MobileNav />
     </>
   );
 };
